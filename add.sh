c78cb5be8d41afca09d4068ae829d74293657b6c
Ojo Oluwasetemi <setemiojo@gmail.com>
2021-03-13 16:45:58 +0100

    📦 NEW: using case , exec and reading input

--- add.sh
+++ add.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-x=12
-y=5
+# x=12
+echo -n "Enter the first value: "
+read x
+echo -n "Enter the second value: "
+read y
+# y=5
 answer=$(echo $(( x + y )))
 echo "The sum of $x and $y is : $answer"
 
@@ -8,3 +12,5 @@ x=5
 y=3
 answer=$(echo $(( x % y )))
 echo "$x mod $y leaves a remainder of : $answer"
+
+echo $0
--- basicArithemetic.sh
+++ basicArithemetic.sh
@@ -3,10 +3,14 @@ read -p "Enter the first number: " num1
 read -p "Enter the second number: " num2
 
 sum=$(( num1 + num2 ))
-echo "$sum"
+# echo "$sum"
+echo "The sum of $num1 and $num2 is : $sum"
 sub=$(( num1 - num2 ))
-echo "$sub"
+# echo "$sub"
+echo "The sub of $num1 and $num2 is : $sub"
 mul=$(( num1 * num2 ))
-echo "$mul"
+# echo "$mul"
+echo "The mul of $num1 and $num2 is : $mul"
 div=$(( num1 / num2 ))
-echo "$div"
+# echo "$div"
+echo "The div of $num1 and $num2 is : $div"
